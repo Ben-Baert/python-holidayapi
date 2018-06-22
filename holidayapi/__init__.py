f117bd04b48b7fd02b7c963681653f131fd72d53
Ben Baert <ben_b@gmx.com>
2018-06-22 14:13:37 +0200

    Works with Python 3

--- holidayapi/__init__.py
+++ holidayapi/__init__.py
@@ -10,14 +10,14 @@ class v1:
     def holidays(self, parameters):
         url = 'https://holidayapi.com/v1/holidays?'
 
-        if parameters.has_key('key') is False:
+        if 'key' not in parameters:
             parameters['key'] = self.key
 
         response = requests.get(url, params=parameters);
         data     = json.loads(response.text)
 
         if response.status_code != 200:
-            if data.has_key('error') is False:
+            if 'error' not in data: 
                 data['error'] = 'Unknown error.'
 
         return data
